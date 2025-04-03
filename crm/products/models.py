6514657a05cf6b9e8fbb1ab6ba840bb1f17485ab
ratmir <ratmir2020.1@gmail.com>
2025-04-03 16:56:48 +0500

    добавил название полей в модели

--- crm/products/models.py
+++ crm/products/models.py
@@ -2,9 +2,19 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(null=False, max_length=150)
-    description = models.TextField(blank=True, null=False, default="", max_length=500)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    name = models.CharField(null=False, max_length=150, verbose_name="Название")
+    description = models.TextField(
+        blank=True,
+        null=False,
+        default="",
+        max_length=500,
+        verbose_name="Описание",
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name="Цена (в руб)",
+    )
 
     def __str__(self):
-        return "Product(pk={}, name={!r}".format(self.pk, self.name)
+        return "Product(pk={}, name={!r})".format(self.pk, self.name)
