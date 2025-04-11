1fd0f90c33874904d60c69e8103dd6b3c04a0331
ratmir <ratmir2020.1@gmail.com>
2025-04-11 16:38:28 +0500

    сделал админку для leads

--- crm/leads/admin.py
+++ crm/leads/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from leads.models import Lead
+
+
+@admin.register(Lead)
+class LeadAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "first_name",
+        "last_name",
+        "phone",
+        "email",
+        "ad",
+    )
+    list_display_links = "pk", "first_name", "last_name"
+    ordering = ("pk",)
+    search_fields = "first_name", "last_name"
